2403190d93efbb232e6af02c7114434d145b6b19
m.jemmoudi <med.jemmoudi@gmail.com>
2019-06-28 14:34:47 +0200

    refactor(app): remove old code & include new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Container } from 'react-bootstrap';
+import TopBar from './components/TopBar';
+import SearchField from './components/SearchField';
+import MoviesList from './components/Movies/Listing';
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TopBar />
+      <Container>
+        <SearchField />
+        <MoviesList />
+      </Container>
     </div>
   );
 }
