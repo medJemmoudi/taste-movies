8067cee43b51de1e4eef46194c6b6fca2189a5c7
m.jemmoudi <med.jemmoudi@gmail.com>
2019-06-28 16:23:59 +0200

    chore(app): adding redux provider & passing app store

--- src/App.tsx
+++ src/App.tsx
@@ -4,16 +4,23 @@ import { Container } from 'react-bootstrap';
 import TopBar from './components/TopBar';
 import SearchField from './components/SearchField';
 import MoviesList from './components/Movies/Listing';
+import { Provider } from 'react-redux';
+import { configureStore } from './configureStore';
+import { Store } from 'redux';
+import { ApplicationState } from './store';
 
 const App: React.FC = () => {
+  const store: Store<ApplicationState> = configureStore();
   return (
-    <div className="App">
-      <TopBar />
-      <Container>
-        <SearchField />
-        <MoviesList />
-      </Container>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <TopBar />
+        <Container>
+          <SearchField />
+          <MoviesList />
+        </Container>
+      </div>
+    </Provider>
   );
 }
 
