349603142c46406d51484338331e3a029ed3b996
med.jemmoudi <med.jemmoudi@gmail.com>
2019-06-29 16:35:25 +0200

    feat(utils): adding helper to make api call using Fetch

---
+++ src/utils/Api.ts
@@ -0,0 +1,13 @@
+export const makeRequest = async (url: string, method: string = 'get', data: Record<string, any> = {}) => {
+    let config: Record<string, any> = { method };
+
+    if (method.toLowerCase() === 'post') {
+        config.body = JSON.stringify(data);
+    }
+
+    // setting headers
+    config.headers = { Accept: 'application/json' };
+
+    const res = await fetch(url, config);
+    return await res.json();
+}
