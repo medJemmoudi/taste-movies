0b7c503b1a9499e6bc032c41118f4411a3a837f7
med.jemmoudi <med.jemmoudi@gmail.com>
2019-06-29 16:38:08 +0200

    chore(deps): adding .env file and Dotenv library

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import dotenv from 'dotenv';
 import './App.scss';
 import * as serviceWorker from './serviceWorker';
+
+dotenv.config();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
