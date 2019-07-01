ebc2023d351a0536667b262eefc58b1dea2b400e
m.jemmoudi <med.jemmoudi@gmail.com>
2019-07-01 11:21:29 +0200

    feat(movies): update movie saga to handle similar movies

--- src/store/movies/saga.ts
+++ src/store/movies/saga.ts
@@ -1,14 +1,11 @@
 import { call, put, takeEvery, all, fork } from 'redux-saga/effects';
-import { makeRequest } from '../../utils/Api';
+import { getSimilarMovies } from '../../utils/Api';
 import { fetchMoviesFailed, fetchMoviesSucces, fetchSimilarMovies } from './actions';
 import { MoviesActionTypes } from './types';
 
-const API_ROUTE: string = 'http://localhost:4000';
-
 function* handleFetchSimilarMovies(action: ReturnType<typeof fetchSimilarMovies>) {
     try {
-        let data = { title: action.payload };
-        let res = yield call(makeRequest, `${API_ROUTE}/similar-to`, 'post', data);
+        let res = yield call(getSimilarMovies, action.payload);
         if (res.error) {
             yield put(fetchMoviesFailed(res.error));
         } else {
--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,3 +1,6 @@
+import cheerio from 'cheerio';
+import { Movie } from '../store/movies/types';
+
 export const makeRequest = async (url: string, method: string = 'get', data: Record<string, any> = {}) => {
     let config: Record<string, any> = { method };
 
@@ -10,4 +13,27 @@ export const makeRequest = async (url: string, method: string = 'get', data: Rec
 
     const res = await fetch(url, config);
     return await res.json();
+}
+
+export const getSimilarMovies = async (movieTitle: string) => {
+    const REACT_APP_PROXY_URL: string = "https://cors-anywhere.herokuapp.com";
+    const tasteApi: string = 'https://tastedive.com/movies/like';
+    const res = await fetch(`${REACT_APP_PROXY_URL}/${tasteApi}/${movieTitle}`);
+    const htmlData = await res.text();
+
+    const $ = cheerio.load(htmlData);
+    const results: Movie[] = [];
+
+    $('.tk-Resource').each((i, el) => {
+        let currentEl = $(el);
+        results.push({
+            poster: currentEl.find('meta[itemprop="image"]').attr('content'),
+            title: currentEl.find('.tk-Resource-title').text(),
+            year: currentEl.find('.tk-Resource-type').text(),
+            likes: currentEl.find('.js-card-likes-counter').text(),
+            rating: currentEl.find('.tk-Rating-score--resource-card').text().trim(),
+        });
+    });
+
+    return results;
 }
