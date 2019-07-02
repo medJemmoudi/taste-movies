99327522ea07ca4b898efec6fb894c8e02880862
m.jemmoudi <med.jemmoudi@gmail.com>
2019-07-02 10:42:16 +0200

    feat(store): combine all sagas into rootSaga

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,12 +3,26 @@ import { combineReducers } from "redux";
 import { MoviesReducer } from "./movies/reducer";
 import { History } from "history";
 import { connectRouter } from "connected-react-router";
+import { MovieDetailsReducer } from "./details/reducer";
+import { MovieDetailsState } from "./details/types";
+import { all, fork } from 'redux-saga/effects';
+import moviesSaga from "./movies/saga";
+import movieDetailsSaga from "./details/saga";
 
 export interface ApplicationState {
-    movies: MoviesState
+    movies: MoviesState,
+    selectedMovie: MovieDetailsState
 }
 
 export const getRootReducer = (history: History) => combineReducers({
     movies: MoviesReducer,
+    selectedMovie: MovieDetailsReducer,
     router: connectRouter(history),
 });
+
+export function* rootSaga() {
+    yield all([
+        fork(moviesSaga),
+        fork(movieDetailsSaga)
+    ]);
+}
