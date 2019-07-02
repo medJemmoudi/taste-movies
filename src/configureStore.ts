e23c0f32d475aab4715e8a85bf82750d30dc444c
m.jemmoudi <med.jemmoudi@gmail.com>
2019-07-02 10:42:36 +0200

    feat(store): add movieDetails to store

--- src/configureStore.ts
+++ src/configureStore.ts
@@ -1,8 +1,7 @@
 import { Store, createStore, applyMiddleware } from "redux";
-import { ApplicationState, getRootReducer } from "./store";
+import { ApplicationState, getRootReducer, rootSaga } from "./store";
 import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
-import moviesSaga from "./store/movies/saga";
 import { History } from 'history';
 import { routerMiddleware } from 'connected-react-router';
 
@@ -13,6 +12,14 @@ export const configureStore = (history: History): Store<ApplicationState> => {
             data: [],
             loading: false,
             error: ''
+        },
+        selectedMovie: {
+            details: {
+                comments: [],
+                synopsis: ""
+            },
+            loading: false,
+            errors: ''
         }
     };
 
@@ -25,7 +32,7 @@ export const configureStore = (history: History): Store<ApplicationState> => {
         applyMiddleware(...middlewares)
     );
 
-    reduxSaga.run(moviesSaga);
+    reduxSaga.run(rootSaga);
 
     return store;
 }
