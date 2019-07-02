38961f6b66a96a204f2cfb389e7afd14a694697c
m.jemmoudi <med.jemmoudi@gmail.com>
2019-07-02 12:09:49 +0200

    feat(component): adding ShowMovieDetails to deal with the selected film

---
+++ src/components/Movies/Details/index.tsx
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+import { MovieDetailsState } from '../../../store/details/types';
+import { ApplicationState } from '../../../store';
+import { fetchMovieDetails } from '../../../store/details/actions';
+import { connect } from 'react-redux';
+import { Row, Col, Image } from 'react-bootstrap';
+import Loading from '../../Common/Loading';
+import _ from 'lodash';
+
+interface IShowMovieDetailsProps {
+    title: string;
+    fetchMovieDetails: Function;
+    selectedMovie: MovieDetailsState
+}
+
+class ShowMovieDetails extends Component<IShowMovieDetailsProps> {
+    componentDidMount() {
+        this.props.fetchMovieDetails(this.props.title);
+    }
+
+    fromatMovieTitle = () => {
+        let splits = _.split(this.props.title, '-');
+        let mapped = _.map(splits, (s) => _.capitalize(s));
+        return mapped.join(' ');
+    }
+
+    showMovie = () => {
+        const { details } = this.props.selectedMovie;
+        return (
+            <>
+                <Col xs={12} sm={12} className="movie-thumbnail">
+                    <Image src={details.poster} rounded />
+                </Col>
+                <Col xs={12} sm={6}>
+                    <h4>{ this.fromatMovieTitle() }</h4>
+                    <ul>
+                        <li><strong>Rating:</strong> { details.rating }</li>
+                        <li><strong>Genre:</strong> { details.genre }</li>
+                        <li><strong>Directed by:</strong> { details.directed_by }</li>
+                        <li><strong>Written by:</strong> { details.written_by }</li>
+                        <li><strong>Studio:</strong> { details.studio }</li>
+                        <li><strong>Box office:</strong> { details.box_office }</li>
+                    </ul>
+                </Col>
+            </>
+        );
+    }
+
+    render() {
+        return (
+            <Row>
+                {this.props.selectedMovie.loading ? <Loading /> : this.showMovie()}
+            </Row>
+        )
+    }
+}
+
+const mapStateToProps = ({ selectedMovie }: ApplicationState) => ({
+    selectedMovie
+});
+
+const mapActionsToProps = { fetchMovieDetails }
+
+export default connect(mapStateToProps, mapActionsToProps)(ShowMovieDetails);
